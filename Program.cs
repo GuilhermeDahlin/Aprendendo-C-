41f49f3221c083073d6f13e1ac335257ebf8020f
GuilhermeDahlin <106753429+GuilhermeDahlin@users.noreply.github.com>
2023-04-20 21:35:41 -0300

    Add files via upload
    Exercitando com mais condicionais e variáveis booleanas

--- Program.cs
+++ Program.cs
@@ -4,34 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _7_Condicionais
+namespace _8_Condicionais2
 {
     class Program
     {
-       //...
         static void Main(string[] args)
         {
-            Console.WriteLine("Executando projeto 7 - Condicionais");
+            Console.WriteLine("Executando projeto 8 - Condicionais 2");
 
             int idadeJoao = 16;
             int quantidadePessoas = 2;
 
-            if (idadeJoao >= 18)
+            bool acompanhado = quantidadePessoas >= 2;
+
+            //1º caso com codicional ou:   if (idadeJoao >= 18 || quantidadePessoas >= 2)
+            //Podemos também fazer o teste com o condicional e (que no CSharp é representado por &&)
+            if (idadeJoao >= 18 && acompanhado == true)
             {
-                Console.WriteLine("João possui mais de 18 anos de idade. Pode entrar.");
+                Console.WriteLine("Pode entrar.");
             }
             else
             {
-                if(quantidadePessoas >= 2)
-                {
-                            Console.WriteLine("João não possui mais de 18 anos, mas está acompanhado. Pode entrar");
-                }
-                    else
-                {                
-                        Console.WriteLine("João não possui mais de 18 anos, não pode entrar");
-                }
+                Console.WriteLine("Não pode entrar");
             }
-
             Console.ReadLine();
         }
     }
